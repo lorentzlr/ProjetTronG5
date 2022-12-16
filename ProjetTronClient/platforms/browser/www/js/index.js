e28ecf7824ceef51521617422aa619bafb3d40b8
Nereisd <dugaleix.nereis@gmail.com>
2022-12-16 08:38:55 +0100

    Update Connexion Serveur

--- ProjetTronClient/platforms/browser/www/js/index.js
+++ ProjetTronClient/platforms/browser/www/js/index.js
@@ -3,6 +3,7 @@ let DOM_reponse = document.getElementById("reponse");
 ws.onopen = function() {
     console.log("Bienvenue sur le jeu Tron")
 };
+<<<<<<< Updated upstream
 ws.onmessage = function(message) {
         DOM_reponse.innerHTML = "Creation de la rom" + message.data
 };
@@ -15,3 +16,42 @@ function connexion() {
     var jsonToString = JSON.stringify(json);
     ws.send(jsonToString);
 }
+=======
+function connection() {
+    let name = document.getElementById('name').value;
+    let password = document.getElementById('password').value;
+
+    let message = {
+        "type" : 'firstConnection',
+        "name" : name,
+        "password" : password
+    };
+
+    console.log(JSON.stringify(message));
+    ws.send(JSON.stringify(message));
+};
+
+ws.onmessage = function(message){
+
+    //On parse le message pour avoir un objet
+    message = JSON.parse(message.data);
+    console.log("Message du serveur : ",message);
+    switch(message.type){
+        case 'firstConnection':
+            //Si c'est false, la connexion a échoué, on va afficher le message du serveur
+            if(message.connectionStatus === false){
+                document.getElementById('messageServeur').innerHTML = message.message
+            } else { //Si c'est true, la connexion a réussi
+                document.getElementById('connection').style.display = 'none'; //On cache le menu de login
+                document.getElementById('waitingRoom').style.display = 'inline-block'; //On affiche la salle d'attente
+
+                //On affiche un petit message concernant la room
+                document.getElementById('infoRoom').innerHTML = "Bienvenue dans la room " + message.idRoom;
+
+                //On stocke les infos de connexion dans le localStorage
+                localStorage.setItem("name", document.getElementById('name').value);
+                localStorage.setItem("password", document.getElementById('password').value);
+            }
+    };
+};
+>>>>>>> Stashed changes
