f9cb15bc27f58be2aa0aaac25e9496cd4db438fc
lorentzlr <romain.lorentz.lr@gmail.com>
2023-01-10 20:53:09 +0100

    Ajout des positions de départ + début de management des déco en cours de partie

---
+++ ProjetTronServeur/www/js/constants.js
@@ -0,0 +1,19 @@
+module.exports = Object.freeze({
+    MAX_USERS_PER_ROOM: 3,
+    MAX_ROOMS: 8,
+    START_POSITIONS: [
+        {
+            x: 0,
+            y: 0
+        },{
+            x: 10,
+            y: 0
+        },{
+            x: 0,
+            y: 10
+        },{
+            x: 10,
+            y: 10
+        },
+    ]
+});
