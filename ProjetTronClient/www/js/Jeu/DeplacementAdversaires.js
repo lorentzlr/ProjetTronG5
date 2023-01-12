c479d2a08ec204a61fb3e3553ce7d06fb50c7da8
Maxime Sage <maxime.sage97@gmail.com>
2023-01-12 11:02:49 +0100

    Modif variable : déplacements adversaires fonctionnels

--- ProjetTronClient/www/js/Jeu/DeplacementAdversaires.js
+++ ProjetTronClient/www/js/Jeu/DeplacementAdversaires.js
@@ -11,17 +11,26 @@ class DeplacementAdversaires {
         }
     }
 
+    /** Déplace la position d'un adversaire donné dans l'objet qui le représente
+     * 
+     * @param {*} adversaire_login le login de l'adversaire
+     * @param {*} position la position renvoyée par le serveur
+     */
     changerPositionAdversaire(adversaire_login, position) {
-       this.adversaires[adversaire_login].setPosition(position);
-       this.deplacerAdversaire(adversaire);
+        this.adversaires[adversaire_login].setPosition(position); //On change sa position
+        this.deplacerAdversaire(adversaire_login); //Et on appelle la fonction pour changer l'affichage du jeu
     }
 
-
+    /** Déplace la position d'un adversaire donné dans l'affichage du jeu en cours
+     * 
+     * @param {*} adversaire le login de l'adversaire
+     */
     deplacerAdversaire(adversaire) {
+        //On vient demander à afficher l'adversaire à la nouvelle positions qu'on a fixée dans l'appel de la fonction précédente
         this.affichage_manager.afficherAdversaire(
             this.plateau.getCases()
-                [this.adversaires[adversaire].getPosition().x]
-                [this.adversaires[adversaire].getPosition().y].getValeur()
+            [this.adversaires[adversaire].getPosition().x]
+            [this.adversaires[adversaire].getPosition().y].getValeur()
         );
     }
 }
