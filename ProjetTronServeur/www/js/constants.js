ea448a4c599575aa11395ebc0d8cba717a4c8e51
Nereisd <dugaleix.nereis@gmail.com>
2023-01-13 11:05:41 +0100

    mise a jour des constantes

--- ProjetTronServeur/www/js/constants.js
+++ ProjetTronServeur/www/js/constants.js
@@ -1,34 +1,34 @@
 module.exports = Object.freeze({
-    MAX_USERS_PER_ROOM: 2,
+    MAX_USERS_PER_ROOM: 4,
     MAX_ROOMS: 8,
     START_POSITIONS: [
         {
             position:
                 {
-                    x: 3,
-                    y: 3
+                    x: 25,
+                    y: 1
                 },
             direction: "right"
         }, {
             position:
                 {
-                    x: 13,
-                    y: 3,
+                    x: 1,
+                    y: 25,
                 },
             direction: "down"
         }, {
             position:
                 {
-                    x: 3,
-                    y: 13,
+                    x: 25,
+                    y: 49,
                 },
             direction: "left"
 
         }, {
             position:
                 {
-                    x: 13,
-                    y: 13,
+                    x: 49,
+                    y: 25,
                 },
             direction: "up"
 
